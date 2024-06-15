a2d5ff53cffddf6a983d19c9b26023535fc7274c
Suraj Gund <surajgund46@gmail.com>
2024-06-15 12:46:04 +0530

    Set Up Done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
